939696c4fcd061f2d292ec6258e9ce1c863c330a
Danial Farid <dan@Danial.local>
2016-02-17 12:38:43 -0500

    fixing entities

---
+++ src/main/java/CompanyBase.java
@@ -0,0 +1,38 @@
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="company")
+public class CompanyBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "id", cascade = CascadeType.PERSIST)
+    private List<Owner> owners;
+
+    public CompanyBase() {
+    }
+
+    public CompanyBase(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
