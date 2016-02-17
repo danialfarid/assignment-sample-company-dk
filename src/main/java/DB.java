cd3f482bda3d4f11886d138e47fb0dcc48c27984
Danial Farid <dan@Danial.local>
2016-02-17 02:10:36 -0500

    fixing entitiess

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,16 +1,25 @@
+import sun.rmi.runtime.Log;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.List;
 import java.util.function.Function;
+import java.util.logging.Logger;
 
 public class DB {
+    private static Logger LOG = Logger.getLogger(DB.class.getName());
     private static final DB INSTANCE = new DB();
     EntityManagerFactory factory = Persistence.createEntityManagerFactory("persistence");
 
     public void createCompany(Company company) {
         withEM(em -> {
-            em.persist(company);
+            try {
+                em.persist(company);
+            } catch (Throwable t) {
+                LOG.throwing("", "", t);
+                LOG.info(t.getMessage());
+            }
             return null;
         });
     }
