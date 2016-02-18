993f19c61f216c8733d06c23e6e5b64e51ee3b84
Danial Farid <dan@Danial.local>
2016-02-18 10:54:38 -0500

    fixing entities

--- web/app/scripts/controllers/main.js
+++ web/app/scripts/controllers/main.js
@@ -10,6 +10,7 @@
 angular.module('webApp')
   .controller('MainCtrl', function ($scope, $resource) {
     var Company = $resource('https://calm-meadow-37274.herokuapp.com/company/:companyId', {companyId:'@id'});
+    var Owner = $resource('https://calm-meadow-37274.herokuapp.com/company/:companyId/owner/:ownerId', {companyId:'@id', ownerId: '@id'});
     $scope.companies = Company.query();
     $scope.currentCompany = {};
     $scope.showDetails = function(company, index) {
@@ -25,4 +26,11 @@ angular.module('webApp')
       }
       console.log(v);
     });
+
+    $scope.addOwner = function(company, owner) {
+      Owner.post({companyId: company.id}, function(fullCompany) {
+        $scope.companies.splice(index, 1, fullCompany);
+        $scope.currentCompany = fullCompany;
+      });
+    };
   });
