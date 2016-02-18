f744764ae4c127090e6f4f676cfdb9dc7bbba8a9
Danial Farid <dan@Danial.local>
2016-02-17 23:08:35 -0500

    fixing entities

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -13,7 +13,7 @@ public class Company implements Serializable {
     @NotNull
     private String name;
 
-    @OneToMany(mappedBy = "company", fetch = FetchType.EAGER, orphanRemoval = true, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "company", fetch = FetchType.EAGER, orphanRemoval = true, cascade = CascadeType.REFRESH)
     @NotEmpty
     private Collection<Owner> owners;
 
